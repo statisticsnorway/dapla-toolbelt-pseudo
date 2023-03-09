ab7a89d52c553ad0ad2617dc4e0c5e039de201f1
Miles Mason Winther <mmw@ssb.no>
2023-03-09 13:52:42 +0100

    Add failing test

--- tests/v1/test_pseudonymize.py
+++ tests/v1/test_pseudonymize.py
@@ -1,8 +1,11 @@
 """Test pseudonymize (v1)"""
 import json
 from unittest import mock
+from mock import patch
 
 import pytest
+import requests
+import pandas as pd
 
 from dapla_pseudo import pseudonymize
 from dapla_pseudo.constants import env
@@ -32,183 +35,218 @@ custom_keyset = PseudoKeyset.parse_obj(
     }
 )
 
+REQUESTS_POST = "requests.post"
+
+
+@pytest.fixture
+def test_data_json_file_path():
+    return "tests/data/personer.json"
+
 
 @mock.patch("dapla.auth.AuthClient")
-def test_pseudonymize_with_default_env_values(patched_auth_client: mock.Mock) -> None:
-    auth_token = "mocked auth token"
+@mock.patch(REQUESTS_POST)
+def test_pseudonymize_with_default_env_values(
+    patched_post: mock.Mock, patched_auth_client: mock.Mock, test_data_json_file_path: str
+) -> None:
     patched_auth_client.fetch_local_user.return_value = {"access_token": auth_token}
 
-    with mock.patch("requests.post") as patched_post:
-        pseudonymize(file_path="tests/data/personer.json", fields=["fnr", "fornavn"])
-        patched_auth_client.called_once()
-        patched_post.assert_called_once()
-        arg = patched_post.call_args.kwargs
+    pseudonymize(file_path=test_data_json_file_path, fields=["fnr", "fornavn"])
+    patched_auth_client.called_once()
+    patched_post.assert_called_once()
+    arg = patched_post.call_args.kwargs
 
-        assert arg["url"] == "http://dapla-pseudo-service.dapla.svc.cluster.local/pseudonymize/file"
-        assert arg["headers"] == {"Authorization": f"Bearer {auth_token}"}
+    assert arg["url"] == "http://dapla-pseudo-service.dapla.svc.cluster.local/pseudonymize/file"
+    assert arg["headers"] == {"Authorization": f"Bearer {auth_token}"}
 
 
-def test_pseudonymize_request_with_default_key(monkeypatch: pytest.MonkeyPatch) -> None:
+@mock.patch("dapla.auth.AuthClient")
+@mock.patch(REQUESTS_POST)
+def test_pseudonymize_dataframe_with_default_env_values(
+    patched_post: mock.Mock, patched_auth_client: mock.Mock, test_data_json_file_path: str
+) -> None:
+    patched_auth_client.fetch_local_user.return_value = {"access_token": auth_token}
+    df = pd.read_json(test_data_json_file_path)
+
+    pseudonymize(df, fields=["fnr", "fornavn"])
+    patched_auth_client.called_once()
+    patched_post.assert_called_once()
+    arg = patched_post.call_args.kwargs
+
+    assert arg["url"] == "http://dapla-pseudo-service.dapla.svc.cluster.local/pseudonymize/file"
+    assert arg["headers"] == {"Authorization": f"Bearer {auth_token}"}
+
+
+@mock.patch(REQUESTS_POST)
+def test_pseudonymize_request_with_default_key(
+    patched_post: mock.Mock, monkeypatch: pytest.MonkeyPatch, test_data_json_file_path: str
+) -> None:
     monkeypatch.setenv(env.PSEUDO_SERVICE_URL, base_url)
     monkeypatch.setenv(env.PSEUDO_SERVICE_AUTH_TOKEN, auth_token)
 
-    with mock.patch("requests.post") as patched:
-        pseudonymize(file_path="tests/data/personer.json", fields=["fnr", "fornavn"])
-        patched.assert_called_once()
-        arg = patched.call_args.kwargs
+    pseudonymize(file_path=test_data_json_file_path, fields=["fnr", "fornavn"])
+    patched_post.assert_called_once()
+    arg = patched_post.call_args.kwargs
 
-        assert arg["url"] == f"{base_url}/pseudonymize/file"
-        assert arg["headers"] == {"Authorization": f"Bearer {auth_token}"}
-        assert arg["stream"] is True
+    assert arg["url"] == f"{base_url}/pseudonymize/file"
+    assert arg["headers"] == {"Authorization": f"Bearer {auth_token}"}
+    assert arg["stream"] is True
 
-        expected_request_json = json.dumps(
-            {
-                "pseudoConfig": {
-                    "rules": [
-                        {"name": "rule-1", "pattern": "**/fnr", "func": "tink-daead(ssb-common-key-1)"},
-                        {"name": "rule-2", "pattern": "**/fornavn", "func": "tink-daead(ssb-common-key-1)"},
-                    ]
-                },
-                "targetContentType": "application/json",
-            }
-        )
-        actual_request_json = find_multipart_obj("request", arg["files"])
-        assert actual_request_json == expected_request_json
+    expected_request_json = json.dumps(
+        {
+            "pseudoConfig": {
+                "rules": [
+                    {"name": "rule-1", "pattern": "**/fnr", "func": "tink-daead(ssb-common-key-1)"},
+                    {"name": "rule-2", "pattern": "**/fornavn", "func": "tink-daead(ssb-common-key-1)"},
+                ]
+            },
+            "targetContentType": "application/json",
+        }
+    )
+    actual_request_json = find_multipart_obj("request", arg["files"])
+    assert actual_request_json == expected_request_json
 
 
 @pytest.mark.parametrize(
     "key", [predefined_keys.SSB_COMMON_KEY_1, predefined_keys.SSB_COMMON_KEY_2, "some-unknown-key"]
 )
-def test_pseudonymize_request_with_explicitly_specified_common_key(monkeypatch: pytest.MonkeyPatch, key: str) -> None:
+@mock.patch(REQUESTS_POST)
+def test_pseudonymize_request_with_explicitly_specified_common_key(
+    patched_post: mock.Mock, monkeypatch: pytest.MonkeyPatch, test_data_json_file_path: str, key: str
+) -> None:
     monkeypatch.setenv(env.PSEUDO_SERVICE_URL, base_url)
     monkeypatch.setenv(env.PSEUDO_SERVICE_AUTH_TOKEN, auth_token)
 
-    with mock.patch("requests.post") as patched:
-        pseudonymize(file_path="tests/data/personer.json", fields=["fnr", "fornavn"], key=key)
-        patched.assert_called_once()
-        arg = patched.call_args.kwargs
+    pseudonymize(file_path=test_data_json_file_path, fields=["fnr", "fornavn"], key=key)
+    patched_post.assert_called_once()
+    arg = patched_post.call_args.kwargs
 
-        assert arg["url"] == f"{base_url}/pseudonymize/file"
-        assert arg["headers"] == {"Authorization": f"Bearer {auth_token}"}
-        assert arg["stream"] is True
+    assert arg["url"] == f"{base_url}/pseudonymize/file"
+    assert arg["headers"] == {"Authorization": f"Bearer {auth_token}"}
+    assert arg["stream"] is True
 
-        expected_request_json = json.dumps(
-            {
-                "pseudoConfig": {
-                    "rules": [
-                        {"name": "rule-1", "pattern": "**/fnr", "func": f"tink-daead({key})"},
-                        {"name": "rule-2", "pattern": "**/fornavn", "func": f"tink-daead({key})"},
-                    ]
-                },
-                "targetContentType": "application/json",
-            }
-        )
-        actual_request_json = find_multipart_obj("request", arg["files"])
-        assert actual_request_json == expected_request_json
+    expected_request_json = json.dumps(
+        {
+            "pseudoConfig": {
+                "rules": [
+                    {"name": "rule-1", "pattern": "**/fnr", "func": f"tink-daead({key})"},
+                    {"name": "rule-2", "pattern": "**/fornavn", "func": f"tink-daead({key})"},
+                ]
+            },
+            "targetContentType": "application/json",
+        }
+    )
+    actual_request_json = find_multipart_obj("request", arg["files"])
+    assert actual_request_json == expected_request_json
 
 
-def test_pseudonymize_request_with_explicitly_specified_keyset(monkeypatch: pytest.MonkeyPatch) -> None:
+@mock.patch(REQUESTS_POST)
+def test_pseudonymize_request_with_explicitly_specified_keyset(
+    patched_post: mock.Mock, monkeypatch: pytest.MonkeyPatch, test_data_json_file_path: str
+) -> None:
     monkeypatch.setenv(env.PSEUDO_SERVICE_URL, base_url)
     monkeypatch.setenv(env.PSEUDO_SERVICE_AUTH_TOKEN, auth_token)
 
     keyset = custom_keyset
 
-    with mock.patch("requests.post") as patched:
-        pseudonymize(file_path="tests/data/personer.json", fields=["fnr", "fornavn"], key=keyset)
-        patched.assert_called_once()
-        arg = patched.call_args.kwargs
+    pseudonymize(file_path=test_data_json_file_path, fields=["fnr", "fornavn"], key=keyset)
+    patched_post.assert_called_once()
+    arg = patched_post.call_args.kwargs
 
-        assert arg["url"] == f"{base_url}/pseudonymize/file"
-        assert arg["headers"] == {"Authorization": f"Bearer {auth_token}"}
-        assert arg["stream"] is True
+    assert arg["url"] == f"{base_url}/pseudonymize/file"
+    assert arg["headers"] == {"Authorization": f"Bearer {auth_token}"}
+    assert arg["stream"] is True
 
-        expected_request_json = json.dumps(
-            {
-                "pseudoConfig": {
-                    "rules": [
-                        {"name": "rule-1", "pattern": "**/fnr", "func": "tink-daead(1234567890)"},
-                        {"name": "rule-2", "pattern": "**/fornavn", "func": "tink-daead(1234567890)"},
-                    ],
-                    "keysets": [
-                        {
-                            "encryptedKeyset": "CiQAp91NBhLdknX3j9jF6vwhdyURaqcT9/M/iczV7fLn...8XYFKwxiwMtCzDT6QGzCCCM=",
-                            "keysetInfo": {
-                                "primaryKeyId": 1234567890,
-                                "keyInfo": [
-                                    {
-                                        "typeUrl": "type.googleapis.com/google.crypto.tink.AesSivKey",
-                                        "status": "ENABLED",
-                                        "keyId": 1234567890,
-                                        "outputPrefixType": "TINK",
-                                    }
-                                ],
-                            },
-                            "kekUri": "gcp-kms://projects/some-project-id/locations/europe-north1/keyRings/some-keyring/cryptoKeys/some-kek-1",
-                        }
-                    ],
-                },
-                "targetContentType": "application/json",
-            }
-        )
-        actual_request_json = find_multipart_obj("request", arg["files"])
-        assert actual_request_json == expected_request_json
+    expected_request_json = json.dumps(
+        {
+            "pseudoConfig": {
+                "rules": [
+                    {"name": "rule-1", "pattern": "**/fnr", "func": "tink-daead(1234567890)"},
+                    {"name": "rule-2", "pattern": "**/fornavn", "func": "tink-daead(1234567890)"},
+                ],
+                "keysets": [
+                    {
+                        "encryptedKeyset": "CiQAp91NBhLdknX3j9jF6vwhdyURaqcT9/M/iczV7fLn...8XYFKwxiwMtCzDT6QGzCCCM=",
+                        "keysetInfo": {
+                            "primaryKeyId": 1234567890,
+                            "keyInfo": [
+                                {
+                                    "typeUrl": "type.googleapis.com/google.crypto.tink.AesSivKey",
+                                    "status": "ENABLED",
+                                    "keyId": 1234567890,
+                                    "outputPrefixType": "TINK",
+                                }
+                            ],
+                        },
+                        "kekUri": "gcp-kms://projects/some-project-id/locations/europe-north1/keyRings/some-keyring/cryptoKeys/some-kek-1",
+                    }
+                ],
+            },
+            "targetContentType": "application/json",
+        }
+    )
+    actual_request_json = find_multipart_obj("request", arg["files"])
+    assert actual_request_json == expected_request_json
 
 
-def test_pseudonymize_request_with_sid(monkeypatch: pytest.MonkeyPatch) -> None:
+@mock.patch(REQUESTS_POST)
+def test_pseudonymize_request_with_sid(
+    patched_post: mock.Mock, monkeypatch: pytest.MonkeyPatch, test_data_json_file_path: str
+) -> None:
     monkeypatch.setenv(env.PSEUDO_SERVICE_URL, base_url)
     monkeypatch.setenv(env.PSEUDO_SERVICE_AUTH_TOKEN, auth_token)
 
-    with mock.patch("requests.post") as patched:
-        pseudonymize(
-            file_path="tests/data/personer.json",
-            fields=[Field(pattern="**/fnr", mapping="sid"), {"pattern": "**/fnr2", "mapping": "sid"}, "fornavn"],
-        )
-        patched.assert_called_once()
-        arg = patched.call_args.kwargs
+    pseudonymize(
+        file_path=test_data_json_file_path,
+        fields=[Field(pattern="**/fnr", mapping="sid"), {"pattern": "**/fnr2", "mapping": "sid"}, "fornavn"],
+    )
+    patched_post.assert_called_once()
+    arg = patched_post.call_args.kwargs
 
-        assert arg["url"] == f"{base_url}/pseudonymize/file"
-        assert arg["headers"] == {"Authorization": f"Bearer {auth_token}"}
-        assert arg["stream"] is True
+    assert arg["url"] == f"{base_url}/pseudonymize/file"
+    assert arg["headers"] == {"Authorization": f"Bearer {auth_token}"}
+    assert arg["stream"] is True
 
-        expected_request_json = json.dumps(
-            {
-                "pseudoConfig": {
-                    "rules": [
-                        {"name": "rule-1", "pattern": "**/fnr", "func": "map-sid(ssb-common-key-1)"},
-                        {"name": "rule-2", "pattern": "**/fnr2", "func": "map-sid(ssb-common-key-1)"},
-                        {"name": "rule-3", "pattern": "**/fornavn", "func": "tink-daead(ssb-common-key-1)"},
-                    ]
-                },
-                "targetContentType": "application/json",
-            }
-        )
-        actual_request_json = find_multipart_obj("request", arg["files"])
-        assert actual_request_json == expected_request_json
+    expected_request_json = json.dumps(
+        {
+            "pseudoConfig": {
+                "rules": [
+                    {"name": "rule-1", "pattern": "**/fnr", "func": "map-sid(ssb-common-key-1)"},
+                    {"name": "rule-2", "pattern": "**/fnr2", "func": "map-sid(ssb-common-key-1)"},
+                    {"name": "rule-3", "pattern": "**/fornavn", "func": "tink-daead(ssb-common-key-1)"},
+                ]
+            },
+            "targetContentType": "application/json",
+        }
+    )
+    actual_request_json = find_multipart_obj("request", arg["files"])
+    assert actual_request_json == expected_request_json
 
 
-def test_pseudonymize_request_with_sid2(monkeypatch: pytest.MonkeyPatch) -> None:
+@mock.patch(REQUESTS_POST)
+def test_pseudonymize_request_using_sid_fields_parameter(
+    patched_post: mock.Mock, monkeypatch: pytest.MonkeyPatch, test_data_json_file_path: str
+) -> None:
     monkeypatch.setenv(env.PSEUDO_SERVICE_URL, base_url)
     monkeypatch.setenv(env.PSEUDO_SERVICE_AUTH_TOKEN, auth_token)
 
-    with mock.patch("requests.post") as patched:
-        pseudonymize(file_path="tests/data/personer.json", fields=["fornavn"], sid_fields=["fnr"])
-        patched.assert_called_once()
-        arg = patched.call_args.kwargs
+    pseudonymize(file_path=test_data_json_file_path, fields=["fornavn"], sid_fields=["fnr"])
+    patched_post.assert_called_once()
+    arg = patched_post.call_args.kwargs
 
-        assert arg["url"] == f"{base_url}/pseudonymize/file"
-        assert arg["headers"] == {"Authorization": f"Bearer {auth_token}"}
-        assert arg["stream"] is True
+    assert arg["url"] == f"{base_url}/pseudonymize/file"
+    assert arg["headers"] == {"Authorization": f"Bearer {auth_token}"}
+    assert arg["stream"] is True
 
-        expected_request_json = json.dumps(
-            {
-                "pseudoConfig": {
-                    "rules": [
-                        {"name": "rule-1", "pattern": "**/fnr", "func": "map-sid(ssb-common-key-1)"},
-                        {"name": "rule-2", "pattern": "**/fornavn", "func": "tink-daead(ssb-common-key-1)"},
-                    ]
-                },
-                "targetContentType": "application/json",
-            }
-        )
-        actual_request_json = find_multipart_obj("request", arg["files"])
-        assert actual_request_json == expected_request_json
+    expected_request_json = json.dumps(
+        {
+            "pseudoConfig": {
+                "rules": [
+                    {"name": "rule-1", "pattern": "**/fnr", "func": "map-sid(ssb-common-key-1)"},
+                    {"name": "rule-2", "pattern": "**/fornavn", "func": "tink-daead(ssb-common-key-1)"},
+                ]
+            },
+            "targetContentType": "application/json",
+        }
+    )
+    actual_request_json = find_multipart_obj("request", arg["files"])
+    assert actual_request_json == expected_request_json
