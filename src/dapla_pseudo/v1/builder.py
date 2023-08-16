fe718140285ca0d635e9ee74d735f5359f641309
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2023-08-16 17:20:27 +0200

    First draft of field based pseudonymization (#187)
    * First draft of field based pseudonymization
    
    * Use valid test FNR's
    
    * Add field metadata model 'PseudoMetadata'
    
    * Pass keyset information to API if supplied
    
    * Run field requests in parallel and store metadata for each response
    
    * Update and add tests
    
    * Move common mock setup of 'do_pseudonymize_field' to a function
    
    * Remove metadata class
    
    * Add docs & ypdate processing of response to match changes in pseudo service
    
    * Revert "Merge branch 'main' into use-field-endpoint"
    
    This reverts commit 70957c744c8c3912f59ec25a3ae820ce1153a107, reversing
    changes made to 3ab150ad23b450ac4886aac9a4e668800fd77b9b.
    
    * Revert "Revert "Merge branch 'main' into use-field-endpoint""
    
    This reverts commit 41f9fbe2995610a26f723c3c305c06ca7af2e09d.
    
    * Add missing timeout
    
    * Update function name
    
    * Update API mock response in test_builder_pandas_pseudonymize_minimal_call
    
    * Simplify _pseudonymize_field by removing a layer of nesting.
    
    * Update cryptography and pygments as recommended by Safety
    
    * Fix error in pseudonymize_field_runner doc
    
    * Add tests for _post_to_field_endpoint
    
    * Strip notebook
    
    * Add from_polars method
    
    * Fix mypy
    
    * Adjust codecov targets
    
    ---------
    
    Co-authored-by: Anders Lunde <anders-lunde@hotmail.com>
    Co-authored-by: Anders Lunde <8294494+Andilun@users.noreply.github.com>

--- src/dapla_pseudo/v1/builder.py
+++ src/dapla_pseudo/v1/builder.py
@@ -1,33 +1,50 @@
 """Builder for submitting a pseudonymization request."""
-from dataclasses import dataclass
+import concurrent
+import json
+import typing as t
 from pathlib import Path
 from typing import Any
 from typing import Optional
 
 import pandas as pd
+import polars as pl
 import requests
 from typing_extensions import Self
 
 from dapla_pseudo.constants import PredefinedKeys
 from dapla_pseudo.constants import PseudoFunctionTypes
-from dapla_pseudo.v1.models import Field
-from dapla_pseudo.v1.models import KeyWrapper
-from dapla_pseudo.v1.models import Mimetypes
-from dapla_pseudo.v1.models import PseudoConfig
 from dapla_pseudo.v1.models import PseudoFunction
-from dapla_pseudo.v1.models import PseudonymizeFileRequest
-from dapla_pseudo.v1.models import PseudoRule
+from dapla_pseudo.v1.models import PseudoKeyset
 from dapla_pseudo.v1.ops import _client
-from dapla_pseudo.v1.ops import _dataframe_to_json
 from dapla_pseudo.v1.supported_file_format import NoFileExtensionError
 from dapla_pseudo.v1.supported_file_format import SupportedFileFormat
 
 
-@dataclass
 class PseudonymizationResult:
     """Holder for data and metadata returned from pseudo-service"""
 
-    dataframe: pd.DataFrame
+    def __init__(self, df: pl.DataFrame, metadata: Optional[t.Dict[str, str]] = None) -> None:
+        """Initialise a PseudonymizationResult."""
+        self._df = df
+        self._metadata = metadata if metadata is not None else {}
+
+    def to_polars(self) -> pl.DataFrame:
+        """Pseudonymized Data as a Polars Dataframe."""
+        return self._df
+
+    def to_pandas(self) -> pd.DataFrame:
+        """Pseudonymized Data as a Pandas Dataframe."""
+        return self._df.to_pandas()
+
+    def get_metadata(self) -> dict[str, str]:
+        """Returns the pseudonymization metadata as a dictionary.
+
+        Returns:
+            Optional[dict[str, str]]: A dictionary containing the pseudonymization metadata,
+            where the keys are field names and the values are corresponding pseudo field metadata.
+            If no metadata is set, returns an empty dictionary.
+        """
+        return self._metadata
 
 
 class PseudoData:
@@ -39,6 +56,11 @@ class PseudoData:
     @staticmethod
     def from_pandas(dataframe: pd.DataFrame) -> "_FieldSelector":
         """Initialize a pseudonymization request from a pandas DataFrame."""
+        return PseudoData._FieldSelector(dataframe)
+
+    @staticmethod
+    def from_polars(dataframe: pl.DataFrame) -> "_FieldSelector":
+        """Initialize a pseudonymization request from a polars DataFrame."""
         return PseudoData._FieldSelector(dataframe)
 
     @staticmethod
@@ -88,23 +110,33 @@ class PseudoData:
     class _FieldSelector:
         """Select one or multiple fields to be pseudonymized."""
 
-        def __init__(self, dataframe: pd.DataFrame):
+        def __init__(self, dataframe: pd.DataFrame | pl.DataFrame):
             """Initialize the class."""
-            self._dataframe = dataframe
+            self._dataframe: pl.DataFrame
+            if isinstance(dataframe, pd.DataFrame):
+                self._dataframe = pl.from_pandas(dataframe)
+            else:
+                self._dataframe = dataframe
 
         def on_field(self, field: str) -> "PseudoData._Pseudonymizer":
             """Specify a single field to be pseudonymized."""
-            return PseudoData._Pseudonymizer(self._dataframe, [Field(pattern=f"**/{field}")])
+            return PseudoData._Pseudonymizer(self._dataframe, [field])
 
         def on_fields(self, *fields: str) -> "PseudoData._Pseudonymizer":
             """Specify multiple fields to be pseudonymized."""
-            return PseudoData._Pseudonymizer(self._dataframe, [Field(pattern=f"**/{f}") for f in fields])
+            return PseudoData._Pseudonymizer(self._dataframe, list(fields))
 
     class _Pseudonymizer:
-        def __init__(self, dataframe: pd.DataFrame, fields: list[Field]) -> None:
-            self._dataframe: pd.DataFrame = dataframe
-            self._fields: list[Field] = fields
+        def __init__(
+            self,
+            dataframe: pl.DataFrame,
+            fields: list[str],
+        ) -> None:
+            self._dataframe: pl.DataFrame = dataframe
+            self._fields: list[str] = fields
             self._pseudo_func: Optional[PseudoFunction] = None
+            self._metadata: t.Dict[str, str] = {}
+            self._pseudo_keyset: Optional[PseudoKeyset] = None
 
         def map_to_stable_id(self) -> Self:
             self._pseudo_func = PseudoFunction(
@@ -113,7 +145,10 @@ class PseudoData:
             return self
 
         def pseudonymize(
-            self, preserve_formatting: bool = False, with_custom_function: Optional[PseudoFunction] = None
+            self,
+            preserve_formatting: bool = False,
+            with_custom_function: Optional[PseudoFunction] = None,
+            with_custom_keyset: Optional[PseudoKeyset] = None,
         ) -> "PseudonymizationResult":
             # If _pseudo_func has been defined upstream, then use that.
             if self._pseudo_func is None:
@@ -133,30 +168,93 @@ class PseudoData:
                     self._pseudo_func = PseudoFunction(
                         function_type=PseudoFunctionTypes.DAEAD, key=PredefinedKeys.SSB_COMMON_KEY_1
                     )
+            if with_custom_keyset is not None:
+                self._pseudo_keyset = with_custom_keyset
 
-            return _do_pseudonymization(dataframe=self._dataframe, fields=self._fields, pseudo_func=self._pseudo_func)
+            return self._pseudonymize_field()
 
+        def _pseudonymize_field(self) -> "PseudonymizationResult":
+            """Pseudonymizes the specified fields in the dataframe using the provided pseudonymization function.
 
-def _do_pseudonymization(
-    dataframe: pd.DataFrame, fields: list[Field], pseudo_func: PseudoFunction
-) -> "PseudonymizationResult":
-    pseudonymize_request = PseudonymizeFileRequest(
-        pseudo_config=PseudoConfig(
-            rules=[
-                PseudoRule(
-                    name=f"{f.pattern.split('/')[-1]}-{pseudo_func}",
-                    pattern=f.pattern,
-                    func=str(pseudo_func),
+            The pseudonymization is performed in parallel. After the parallel processing is finished,
+            the pseudonymized fields replace the original fields in the dataframe stored in `self._dataframe`.
+
+            Returns:
+                PseudonymizationResult: Containing the pseudonymized 'self._dataframe' and the associated metadata.
+            """
+
+            def pseudonymize_field_runner(field_name: str, series: pl.Series) -> tuple[str, pl.Series]:
+                """Function that performs the pseudonymization on a pandas Series.
+
+                Args:
+                    series (pl.Series): The pandas Series containing the values to be pseudonymized.
+                    field_name (str):  The name of the field.
+
+                Returns:
+                    tuple[str,pl.Series]: A tuple containing the field_name and the corresponding series.
+                """
+                return (
+                    field_name,
+                    _do_pseudonymize_field(
+                        path="pseudonymize/field",
+                        field_name=field_name,
+                        values=series.to_list(),
+                        pseudo_func=self._pseudo_func,
+                        metadata_map=self._metadata,
+                        keyset=self._pseudo_keyset,
+                    ),
                 )
-                for f in fields
-            ],
-            keysets=KeyWrapper(pseudo_func.key).keyset_list(),
-        ),
-        target_content_type=Mimetypes.JSON,
-        target_uri=None,
-        compression=None,
+
+            # Execute the pseudonymization API calls in parallel
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                pseudonymized_field: t.Dict[str, pl.Series] = {}
+                futures = [
+                    executor.submit(pseudonymize_field_runner, field, self._dataframe[field]) for field in self._fields
+                ]
+                # Wait for the futures to finish, then add each field to pseudonymized_field map
+                for future in concurrent.futures.as_completed(futures):
+                    result = future.result()
+                    # Each future result contains the field_name (0) and the pseudonymize_values (1)
+                    pseudonymized_field[result[0]] = result[1]
+
+                pseudonymized_df = pl.DataFrame(pseudonymized_field)
+                self._dataframe = self._dataframe.update(pseudonymized_df)
+
+            return PseudonymizationResult(df=self._dataframe, metadata=self._metadata)
+
+
+def _do_pseudonymize_field(
+    path: str,
+    field_name: str,
+    values: list[str],
+    pseudo_func: Optional[PseudoFunction],
+    metadata_map: t.Dict[str, str],
+    keyset: Optional[PseudoKeyset] = None,
+) -> pl.Series:
+    """Makes pseudonymization API calls for a list of values for a specific field and processes it into a polars Series.
+
+    Args:
+        path (str): The path to the pseudonymization endpoint.
+        field_name (str): The name of the field being pseudonymized.
+        values (list[str]): The list of values to be pseudonymized.
+        pseudo_func (Optional[PseudoFunction]): The pseudonymization function to apply to the values.
+        metadata_map (Dict[str, str]): A dictionary to store the metadata associated with each field.
+        keyset (Optional[PseudoKeyset], optional): The pseudonymization keyset to use. Defaults to None.
+
+    Returns:
+        pl.Series: A pandas Series containing the pseudonymized values.
+    """
+    response: requests.Response = _client()._post_to_field_endpoint(
+        path, field_name, values, pseudo_func, keyset, stream=True
     )
-    response: requests.Response = _client().pseudonymize(
-        pseudonymize_request, _dataframe_to_json(dataframe), timeout=None, stream=True
-    )
-    return PseudonymizationResult(dataframe=pd.json_normalize(response.json()))
+    metadata_map[field_name] = str(response.headers.get("metadata") or "")
+
+    # The response content is received as a buffered byte stream from the server.
+    # We decode the content using UTF-8, which gives us a List[List[str]] structure.
+    # To obtain a single list of strings, we combine the values from the nested sublists into a flat list.
+    nested_list = json.loads(response.content.decode("utf-8"))
+    combined_list = []
+    for sublist in nested_list:
+        combined_list.extend(sublist)
+
+    return pl.Series(combined_list)
