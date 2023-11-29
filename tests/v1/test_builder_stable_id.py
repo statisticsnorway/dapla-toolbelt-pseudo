80a03240c8d185d463027323e29a070394f34315
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2023-11-29 15:11:02 +0100

    Fixed test

--- tests/v1/test_builder_stable_id.py
+++ tests/v1/test_builder_stable_id.py
@@ -15,7 +15,7 @@ TEST_FILE_PATH = "tests/v1/test_files"
 
 @pytest.fixture()
 def df() -> pd.DataFrame:
-    with open("../../tests/data/personer.json") as test_data:
+    with open("tests/data/personer.json") as test_data:
         return pd.json_normalize(json.load(test_data))
 
 
@@ -30,7 +30,7 @@ def sid_lookup_missing_response() -> MagicMock:
 
 
 @patch("dapla_pseudo.v1.PseudoClient._post_to_sid_endpoint")
-def test_builder_can_map_to_stable_id(
+def test_builder_validate_map_to_stable_id(
     patched_post_to_sid_endpoint: Mock, df: pd.DataFrame, sid_lookup_missing_response: MagicMock
 ) -> None:
     field_name = "fnr"
