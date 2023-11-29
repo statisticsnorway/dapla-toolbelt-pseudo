9408ed4ddc71dc68d9827cecf1c6be15dd6ab96b
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2023-11-29 15:39:08 +0100

    Fix types for dict

--- src/dapla_pseudo/types.py
+++ src/dapla_pseudo/types.py
@@ -10,7 +10,7 @@ import pandas as pd
 from dapla_pseudo.v1.models import Field
 
 
-FieldDecl = t.Union[str, dict, Field]
+FieldDecl = t.Union[str, dict[str, str], Field]
 BinaryFileDecl = t.Union[t.BinaryIO, io.BufferedReader, fsspec.spec.AbstractBufferedFile]
 DatasetDecl = t.Union[pd.DataFrame, BinaryFileDecl, str, Path]
 FileSpecDecl = tuple[t.Optional[str], t.Union[BinaryFileDecl, str], str]
