e08487e7deb7cd92c8ed428bf13c7e6072839573
Michael Moen Allport <mmallport@gmail.com>
2023-12-14 11:48:34 +0100

    Add result class for files

--- src/dapla_pseudo/v1/builder_models.py
+++ src/dapla_pseudo/v1/builder_models.py
@@ -4,6 +4,8 @@ from typing import Optional
 
 import pandas as pd
 import polars as pl
+from requests import Response
+from dapla import FileClient
 
 
 class DataFrameResult:
@@ -21,6 +23,41 @@ class DataFrameResult:
     def to_pandas(self) -> pd.DataFrame:
         """Pseudonymized Data as a Pandas Dataframe."""
         return self._df.to_pandas()
+
+    @property
+    def metadata(self) -> dict[str, str]:
+        """Returns the pseudonymization metadata as a dictionary.
+
+        Returns:
+            Optional[dict[str, str]]: A dictionary containing the pseudonymization metadata,
+            where the keys are field names and the values are corresponding pseudo field metadata.
+            If no metadata is set, returns an empty dictionary.
+        """
+        return self._metadata
+
+
+class FileResult:
+    def __init__(
+        self, dataset_response: Response, metadata: Optional[t.Dict[str, str]] = None, streamed: bool = False
+    ) -> None:
+        """Initialise a FileResult."""
+        self._dataset_response = dataset_response
+        self._metadata = metadata if metadata else {}
+        self._streamed = streamed
+
+    def to_file(self, file_path: str) -> None:
+        if file_path.startswith("gs://"):
+            file_handle = FileClient().gcs_open(file_path, mode="wb")
+        else:
+            file_handle = open(file_path, mode="wb")
+
+        if self._streamed:
+            for chunk in self._dataset_response.iter_content(chunk_size=128):
+                file_handle.write(chunk)
+        else:
+            file_handle.write(self._dataset_response.content)
+
+        file_handle.close()
 
     @property
     def metadata(self) -> dict[str, str]:
