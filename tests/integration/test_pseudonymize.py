d8541b86b4ebe2558b1ba134a9f929781e4ea8cf
Andilun <8294494+Andilun@users.noreply.github.com>
2024-02-02 13:32:29 +0100

    Fix result df

--- tests/integration/test_pseudonymize.py
+++ tests/integration/test_pseudonymize.py
@@ -16,18 +16,14 @@ def test_pseudonymize() -> None:
             "fodselsdato": "string",
         },
     )
-    expected_data = {
-        "fnr": [
-            "AWIRfKIwWmlxO9zxQ7DM0a31ZDCpfHTg…",
-            "AWIRfKLuAuP/Y0C3MAXO8eXIgDIX7Ss1…",
-            "AWIRfKIDQ0LedgTUaq7GVql7ZwVZeVC/…",
-        ],
-        "fornavn": ["Donald", "Mikke", "Anton"],
-        "etternavn": ["Duck", "Mus", "Duck"],
-        "kjonn": ["M", "M", "M"],
-        "fodselsdato": ["020995", "060970", "180999"],
-    }
-    expected_df = pd.DataFrame(expected_data)
+
+    expected_result_fnr = [
+        "AWIRfKIwWmlxO9zxQ7DM0a31ZDCpfHTg…",
+        "AWIRfKLuAuP/Y0C3MAXO8eXIgDIX7Ss1…",
+        "AWIRfKIDQ0LedgTUaq7GVql7ZwVZeVC/…",
+    ]
+    expected_result_df = df.copy()
+    expected_result_df["fnr"].replace(expected_result_fnr)
 
     result = (
         Pseudonymize.from_pandas(df)  ### GENERAL WORKFLOW ###  # Select dataset
@@ -35,4 +31,4 @@ def test_pseudonymize() -> None:
         .with_default_encryption()  # Select encryption method on fields
         .run()  # Apply pseudonymization
     )
-    assert result.to_pandas().equals(expected_df)
+    assert result.to_pandas().equals(expected_result_df)
