f7661be69467ae2569b60542bb5eeed3adfd6b42
Andilun <8294494+Andilun@users.noreply.github.com>
2024-02-16 16:39:41 +0100

    Remove test case for depseudo of a combination of pseudonymized and unpseudonymized columns
    The behavior seems to be unintended, tests can be added for this when expected behavior is better documented.

--- tests/integration/test_integration_deseudonymize.py
+++ tests/integration/test_integration_deseudonymize.py
@@ -1,8 +1,6 @@
 from collections.abc import Generator
 
 import polars as pl
-import pytest
-from requests import HTTPError
 
 from dapla_pseudo import Depseudonymize
 from tests.integration.utils import df_personer
@@ -25,24 +23,3 @@ def test_depseudonymize_default_encryption(
         .to_polars()
     )
     assert result.equals(df_personer)
-
-
-@integration_test()
-def test_depseudonymize_default_encryption_null(
-    setup: Generator[None, None, None],
-    df_personer: pl.DataFrame,
-    df_personer_fnr_daead_encrypted: pl.DataFrame,
-) -> None:
-    new_data = pl.DataFrame({"fnr": df_personer["fnr"].to_list()})
-    df_personer_fnr_daead_encrypted = df_personer_fnr_daead_encrypted.update(new_data)
-
-    with pytest.raises(HTTPError) as http_error:
-        (
-            Depseudonymize.from_polars(df_personer_fnr_daead_encrypted)
-            .on_fields("fnr", "fornavn", "etternavn", "kjonn", "fodselsdato")
-            .with_default_encryption()
-            .run()
-            .to_polars()
-        )
-
-        assert http_error.response.status_code == 500  # type: ignore
