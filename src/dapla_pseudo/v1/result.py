d8daa63be698275a5ec613dc4aa97ac12ee45250
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2024-03-27 13:35:53 +0100

    Remove special handling of metadata from map-sid function (fix #363).

--- src/dapla_pseudo/v1/result.py
+++ src/dapla_pseudo/v1/result.py
@@ -198,28 +198,9 @@ class Result:
         self,
         raw_metadata: list[dict[str, Any]],
     ) -> t.Optional[PseudoVariable]:
-        if len(raw_metadata) == 1:  # Only one element in list if NOT using SID-mapping
-            return PseudoVariable.model_validate(raw_metadata[0])
-        elif len(raw_metadata) == 2 and any(
-            "stable_identifier_type" in pseudo_var for pseudo_var in raw_metadata
-        ):  # SID-mapping
-            sid_metadata = next(
-                pseudo_var
-                for pseudo_var in raw_metadata
-                if "stable_identifier_type" in pseudo_var
-            )
-            encrypt_metadata = next(
-                pseudo_var
-                for pseudo_var in raw_metadata
-                if "stable_identifier_type" not in pseudo_var
-            )
-            pseudo_variable = PseudoVariable.model_validate(encrypt_metadata)
-            pseudo_variable.stable_identifier_type = sid_metadata[
-                "stable_identifier_type"
-            ]
-            pseudo_variable.stable_identifier_version = sid_metadata[
-                "stable_identifier_version"
-            ]
-            return pseudo_variable
-        else:
+        if len(raw_metadata) == 0:
             return None
+        elif len(raw_metadata) > 1:
+            print(f"Unexpected length of metadata: {len(raw_metadata)}")
+        return PseudoVariable.model_validate(raw_metadata[0])
+
