e629ac8924a76a647eedc1a587e319752cac865a
Michael Moen Allport <mmallport@gmail.com>
2024-04-04 15:42:39 +0200

    Explicitly denote type aliases

--- src/dapla_pseudo/types.py
+++ src/dapla_pseudo/types.py
@@ -2,6 +2,7 @@
 
 import io
 from pathlib import Path
+from typing import TypeAlias
 
 import fsspec
 import gcsfs
@@ -9,14 +10,14 @@ import pandas as pd
 
 from dapla_pseudo.v1.api_models import Field
 
-FieldDecl = str | dict[str, str] | Field
-BinaryFileDecl = (
+FieldDecl: TypeAlias = str | dict[str, str] | Field
+BinaryFileDecl: TypeAlias = (
     io.BufferedReader | fsspec.spec.AbstractBufferedFile | gcsfs.core.GCSFile
 )
 
-DatasetDecl = pd.DataFrame | BinaryFileDecl | str | Path
-FileLikeDatasetDecl = BinaryFileDecl | str | Path
-FileSpecDecl = tuple[str | None, BinaryFileDecl | str, str]
+DatasetDecl: TypeAlias = pd.DataFrame | BinaryFileDecl | str | Path
+FileLikeDatasetDecl: TypeAlias = BinaryFileDecl | str | Path
+FileSpecDecl: TypeAlias = tuple[str | None, BinaryFileDecl | str, str]
 # FileSpecDecl is derived from the "files" argument in multi-part requests from the "Requests"-library
 # The tuple semantically means: ('filename', fileobj, 'content_type')
 # See "files" in https://requests.readthedocs.io/en/latest/api/#requests.request
