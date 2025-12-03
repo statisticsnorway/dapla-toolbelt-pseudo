9c5d26a2171ec0378abbc2afca8a30707658f871
Michael Moen Allport <mmallport@gmail.com>
2025-12-03 16:29:19 +0100

    fix mypy

--- tests/v1/integration/test_result.py
+++ tests/v1/integration/test_result.py
@@ -28,7 +28,10 @@ def test_repseudonymize_with_metadata(
         .to_papis_compatible_encryption()
         .run()
     )
-    datadoc_variables: dict[str, t.Any] = result.datadoc_model["datadoc"]["variables"]
+    assert isinstance(result.datadoc_model, dict)
+    datadoc_variables: list[dict[str, t.Any]] = result.datadoc_model["datadoc"][
+        "variables"
+    ]
     if fnr_variable := next(
         filter(lambda v: v["short_name"] == "fnr", datadoc_variables), None
     ):
@@ -62,7 +65,10 @@ def test_depseudonymize_with_metadata(
         .run()
     )
     # The index 0 here corresponds to the variable 'fnr'
-    datadoc_variables: dict[str, t.Any] = result.datadoc_model["datadoc"]["variables"]
+    assert isinstance(result.datadoc_model, dict)
+    datadoc_variables: list[dict[str, t.Any]] = result.datadoc_model["datadoc"][
+        "variables"
+    ]
     if fnr_variable := next(
         filter(lambda v: v["short_name"] == "fnr", datadoc_variables), None
     ):
@@ -85,7 +91,10 @@ def test_pseudonymize_with_metadata(
         .with_default_encryption()
         .run()
     )
-    datadoc_variables: dict[str, t.Any] = result.datadoc_model["datadoc"]["variables"]
+    assert isinstance(result.datadoc_model, dict)
+    datadoc_variables: list[dict[str, t.Any]] = result.datadoc_model["datadoc"][
+        "variables"
+    ]
     if fnr_variable := next(
         filter(lambda v: v["short_name"] == "fnr", datadoc_variables), None
     ):
